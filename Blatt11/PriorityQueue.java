316259431e723e574a6c0569898eb474c4e945be
The-Ludwig <ludwig.neste@tu-dortmund.de>
2018-06-27 11:34:20 +0200

    comments

--- Blatt11/PriorityQueue.java
+++ Blatt11/PriorityQueue.java
@@ -4,12 +4,14 @@ class PriorityQueue
 {
 	public static void main(String[] args) 
 	{
+		//self explaining
 		if(args.length != 2)
 		{
 			System.out.println("You have to give 2 arguments (ints)");
 			return;
 		}
 		int n = -1, k = -1;
+		//self explaining
 		try
 		{
 			n = Integer.parseInt(args[0]);
@@ -19,12 +21,16 @@ class PriorityQueue
 			return;
 		}
 		
+		//heap and random generator 
 		MaxHeap heap = new MaxHeap(n+k);
 		Random r = new Random();
 		
+		
+		//fill heap
 		for(int i = 0; i < n; i++) 
 			heap.insert(r.nextInt(100));
 		
+		//do operations on hepa
 		for(int i = 0; i < k; i++)
 		{
 			if(r.nextInt(4) < 3)
